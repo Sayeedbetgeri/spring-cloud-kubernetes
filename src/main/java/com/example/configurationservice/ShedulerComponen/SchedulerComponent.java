b62bd96e93c1dc0e06aaec1477625048d31e78ec
Betgeri, Sayeedahmed <BetgeriS@aimspecialtyhealth.com>
2021-10-08 13:52:03 +0530

    adding scheduler

---
+++ src/main/java/com/example/configurationservice/ShedulerComponen/SchedulerComponent.java
@@ -0,0 +1,19 @@
+package com.example.configurationservice.ShedulerComponen;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedulerComponent {
+
+
+    @Value("${message}")
+    private String message;
+
+    @Scheduled(fixedDelay = 3000)
+    public void display()
+    {
+        System.out.println("Hello : "+ message);
+    }
+}
