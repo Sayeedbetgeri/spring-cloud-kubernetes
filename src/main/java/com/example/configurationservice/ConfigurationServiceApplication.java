d435877b93caff56302d772617e21d9ee68bba61
Betgeri, Sayeedahmed <BetgeriS@aimspecialtyhealth.com>
2021-10-08 14:01:31 +0530

    adding scheduler

--- src/main/java/com/example/configurationservice/ConfigurationServiceApplication.java
+++ src/main/java/com/example/configurationservice/ConfigurationServiceApplication.java
@@ -19,9 +19,11 @@ package com.example.configurationservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
+@EnableScheduling
 public class ConfigurationServiceApplication {
 
     public static void main(String[] args) {
